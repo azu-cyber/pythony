e9c97d33ed6874e9248fee17bbbd2315dd84c453
azu-cyber <avikalizhimomi@gmail.com>
2024-11-13 17:30:07 +0530

    GCD

--- A game/main.py
+++ A game/main.py
@@ -9,3 +9,10 @@ if original_number == reversed_number:
     print(f"{original_number} is a palindrome*")
 else:
     print(f"{original_number} is not a palindrome*")
+numberLargest=int(input("Enter Largest number: "))
+numberSmallest=int(input("Enter Smallest number: "))
+while(numberSmallest):
+    numberStore = numberSmallest
+    numberSmallest = numberLargest % numberSmallest
+    numberLargest = numberStore
+print("HCF is : ",numberLargest)
